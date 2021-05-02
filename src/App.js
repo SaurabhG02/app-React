5d87adbd019ed61cbee34a1cc0650a382fa2a796
SaurabhG02 <59133760+SaurabhG02@users.noreply.github.com>
2021-05-02 19:52:16 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <nav className={classes.Topbar}>
+          <img src="https://www.logo.wine/a/logo/Amazon_(company)/Amazon_(company)-Logo.wine.svg" alt="Amazon Logo" />
+        </nav>
       </header>
+      <div className={classes.MainContainer}>
+        <div className={classes.ProductPreview}>
+          <img src="https://imgur.com/iOeUBV7.png" alt="Product Preview" />
+        </div>
+
+      </div>
     </div>
   );
 }
