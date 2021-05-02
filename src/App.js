bacf03c74e0c1560ff25805715b3be7cc0fb53d3
SaurabhG02 <59133760+SaurabhG02@users.noreply.github.com>
2021-05-02 20:56:10 +0530

    features

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import classes from './App.module.css';
+import ProductData from './ProductData'
 
 function App() {
   return (
@@ -12,6 +13,23 @@ function App() {
       <div className={classes.MainContainer}>
         <div className={classes.ProductPreview}>
           <img src="https://imgur.com/iOeUBV7.png" alt="Product Preview" />
+        </div>
+        <div className={classes.Productdata}>
+          <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
+          <p className={classes.ProductDescription}>{ProductData.description}</p>
+
+          <h3 className={classes.SectionHeading}>Select Color</h3>
+          <div>
+            <img className={[classes.ProductImage, classes.SelectedProductImage].join(' ')} src="https://imgur.com/PTgQlim.png" alt="red Colourd" />
+            <img className={classes.ProductImage} src="https://imgur.com/iOeUBV7.png" alt="black Colourd" />
+            <img className={classes.ProductImage} src="https://imgur.com/Mplj1YR.png" alt="blue Colourd" />
+            <img className={classes.ProductImage} src="https://imgur.com/xSIK4M8.png" alt="purple Colourd" />
+          </div>
+          <h3 className={classes.SectionHeading}>Features</h3>
+          <div>
+            <button className={classes.FeatureItem}>Time</button>
+            <button className={classes.FeatureItem}>Heart Rate</button>
+          </div>
         </div>
 
       </div>
